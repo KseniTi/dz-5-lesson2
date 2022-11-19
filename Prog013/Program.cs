889b8a8b939fac5e93eb92bc88371959e4468d6c
KseniTi <lioness2006@bk.ru>
2022-11-19 18:31:08 +0300

    Prog013

---
+++ Prog013/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 13: Напишите программу, которая с помощью
+деления выводит третью цифру заданного числа или
+сообщает, что третьей цифры нет.*/
+
+void Zadacha13()
+{
+    Console.WriteLine("Введите трёхзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else if (number < 1000)
+    {
+        Console.WriteLine(number % 10);
+    }
+    else{
+        Console.WriteLine("Не знаю");
+    }
+
+}
+
+Zadacha13();
