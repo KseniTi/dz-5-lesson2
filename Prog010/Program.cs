96b5114fd793587ff534beeaf7d2881cc141dcfd
KseniTi <lioness2006@bk.ru>
2022-11-19 18:22:49 +0300

    Prog010

---
+++ Prog010/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Zadacha10()
+{
+    Console.WriteLine("Введите трёхзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int a = number / 100;
+    int b = number / 10;
+    int c = b - a * 10;
+    
+    Console.WriteLine($"{c}");
+
+}
+
+Zadacha10();
